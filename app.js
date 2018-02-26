cb795b2efb025a4116bebe1e2b163b76398923a5
wScottSh <7772230+wScottSh@users.noreply.github.com>
2018-02-26 10:56:31 -0600

    adds time packages

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-var users = require('./routes/users');
+// first use local ENV, else hit the remote one
+let ENV;
+try {
+  ENV = require('./env');
+} catch (ex) {
+  ENV = process.env;
+}
 
-var app = express();
+const users = require('./routes/users');
+
+const app = express();
+// connects react app to the backend
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
+
+mongoose.connect(ENV.MONGODB_URI);
 
 app.use('/api/v1/users', users);
 
